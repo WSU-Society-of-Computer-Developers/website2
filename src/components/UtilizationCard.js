9d8c7ed7b0d51fe6c55164c2b2bd3dcb31a6ee74
Zavaar Shah <thatziv@gmail.com>
2022-08-30 22:21:45 -0400

    Team member page redesign
    * Finished team member page
    * Team member grid was refactored with responsive to screen size changes
    * Team member data was added
    
    TODO:
    
    * Add yousra and oke images
    * add bio instead of major property? SEE /src/data/index.js#L43
    * Complete rest of project
    	- Donate page
    	- Gallery page
    	- Contact us form page
    	- Link events from rss feed from get involved
    	- embed scd calander on website ???

--- src/components/UtilizationCard.js
+++ src/components/UtilizationCard.js
@@ -4,18 +4,26 @@ import { StatusBadge, Avatar } from "../components";
 
 import { Mail } from "grommet-icons"
 
-export const UtilizationCard = ({ data, url, ...rest }) => (
+export const UtilizationCard = ({ data, url, gridArea, ...rest }) => (
   <Box
-    direction="row"
+    direction="column"
     justify="between"
-    gap="large"
+    gap="small"
     pad="medium"
     background="black"
     round="small"
     className="animate__animated animate__zoomIn"
-    style={{ marginBottom: 10, }}
+    style={{ marginBottom: 10 }}
   >
-    <Box gap="large">
+    <Box gap="small">
+      {/* pic */}
+      <Box
+        height="small"
+        width="small"
+        round="small"
+      >
+        <Image style={{ borderRadius: 15 }} fit="cover" src={data.img || "/ppl/placeholder.jpg"} />
+      </Box>
       <Box gap="xsmall">
         <Heading level="2" margin="none" size="small">
           {data.name}
@@ -26,16 +34,8 @@ export const UtilizationCard = ({ data, url, ...rest }) => (
         <Text color="light-4" size="small">
           {data.major}
         </Text>
-        {data.contact && <Button secondary href={data.contact} target="_blank"><Mail/></Button>} 
+        {data.contact && <Button secondary href={data.contact} target="_blank"><Mail /></Button>}
       </Box>
-    </Box>
-    {/* pic */}
-    <Box
-      height="small"
-      width="small"
-      round="small"
-    >
-      <Image style={{ borderRadius: 15}} fit="cover" src={data.img} />
     </Box>
   </Box>
 );
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -4,13 +4,117 @@ import ReactDOM from "react-dom";
 import { Box, Grid, ResponsiveContext, Heading, Grommet } from "grommet";
 import { deepMerge } from "grommet/utils";
 import { grommet } from "grommet/themes";
-
 import React, { Component } from "react";
 import { VirtualMachinesCard, UtilizationCard } from "../components";
+
+const theme = deepMerge({
+    global: {
+        breakpoints: {
+            small: {
+                value: 450
+            },
+            medium: {
+                value: 655
+            },
+            large: { value: 19999 }
+        }
+    }
+});
+
+const columns = {
+    small: ["auto"],
+    medium: ["auto", "auto"],
+    large: ["auto", "auto"],
+    xlarge: ["auto", "auto"]
+};
+
+
+const rows = {
+    small: ["medium", "medium", "medium"],
+    medium: ["medium", "medium"],
+    large: ["medium"],
+    xlarge: ["medium"]
+};
+
+const ResponsiveGrid = ({
+    children,
+    overrideColumns,
+    overrideRows,
+    areas,
+    ...props
+}) => (
+    <ResponsiveContext.Consumer>
+        {size => {
+            // take into consideration if not array is sent but a simple string
+            let columnsVal = columns;
+            if (columns) {
+                if (columns[size]) {
+                    columnsVal = columns[size];
+                }
+            }
+
+            let rowsVal = rows;
+            if (rows) {
+                if (rows[size]) {
+                    rowsVal = rows[size];
+                }
+            }
+
+            // also if areas is a simple array not an object of arrays for different sizes
+            let areasVal = areas;
+            if (areas && !Array.isArray(areas)) areasVal = areas[size];
+
+            return (
+                <Grid
+                    {...props}
+                    areas={!areasVal ? undefined : areasVal}
+                    rows={!rowsVal ? size : rowsVal}
+                    columns={!columnsVal ? size : columnsVal}
+                >
+                    {children}
+                </Grid>
+            );
+        }}
+    </ResponsiveContext.Consumer>
+);
+
+
+const areas = {
+    small: [
+        { name: "header", start: [0, 0], end: [0, 0] },
+        { name: "test", start: [0, 1], end: [0, 1] },
+        { name: "test1", start: [0, 2], end: [0, 2] }
+    ],
+    medium: [
+        { name: "header", start: [0, 0], end: [1, 0] },
+        { name: "test", start: [0, 1], end: [0, 1] },
+        { name: "test1", start: [1, 1], end: [1, 1] }
+    ],
+    large: [
+        { name: "header", start: [0, 0], end: [0, 0] },
+        { name: "test", start: [1, 0], end: [1, 0] },
+        { name: "test1", start: [2, 0], end: [2, 0] }
+    ],
+    xlarge: [
+        { name: "header", start: [0, 0], end: [0, 0] },
+        { name: "test", start: [1, 0], end: [1, 0] },
+        { name: "test1", start: [2, 0], end: [2, 0] }
+    ]
+};
+
 export default function Team() {
     return <Box alignSelf="center" style={{ marginTop: 25 }} align="center">
-        {teamMembers.map((member) => <UtilizationCard data={{ ...member }} />
-        )}
+        <Grommet theme={theme}>
+            <ResponsiveGrid
+                rows={rows}
+                columns={columns}
+                gap="small"
+                areas={areas}
+                margin="medium"
+            >
+                {teamMembers.map((member) => <UtilizationCard data={{ ...member }} />)}
+            </ResponsiveGrid>
+        </Grommet>
     </Box>
 }
 
