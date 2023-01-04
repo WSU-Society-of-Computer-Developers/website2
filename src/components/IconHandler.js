9f7f43d7499508c7015e69dd701de63cf8dd5a31
Zavaar Shah <thatziv@gmail.com>
2023-01-03 19:26:36 -0500

    built new theme and changed entire color scheme

--- src/components/IconHandler.js
+++ src/components/IconHandler.js
@@ -4,7 +4,7 @@ import { Box, Tip } from "grommet"
 export default ({ name, icon, showName, url }) => {
     return <Box pad="medium">
         <Tip content={name} dropProps={{trapFocus: true}}>
-            <a style={{fontSize: "larger", color: ""}} target="_blank" rel="noopener noreferrer" href={url || '/#'}>
+            <a style={{fontSize: "larger"}} target="_blank" rel="noopener noreferrer" href={url || '/#'}>
                 <span style={{display: "none"}}>{name}</span>
                 <i className={icon + " paypal-donate"}></i>{showName && name}
             </a>
