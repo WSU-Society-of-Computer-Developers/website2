e61726098a5448ee1e55c5590fff0ebebe22feac
Zavaar Shah <thatziv@gmail.com>
2023-01-05 19:56:54 -0500

    directly embedded scribe instructions to donate

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -3,13 +3,13 @@ import { Box, Text, Heading, Image, Button, Paragraph } from "grommet";
 import { StatusBadge, Avatar } from "../components";
 import PaypalEmbed from "../components/PaypalEmbed";
 
-
 export default function Donate() {
     return (
         <Box align="center" height="100%" gap="small" pad="small" className="animate__animated animate__fadeInDown animate__fast" style={{ color: "whitesmoke" }}>
             <Heading className="title">Donate</Heading>
             <a target="_blank" href="donoguide.pdf"><Button primary label="Get PDF Instructions"/></a>
-            <iframe style={{maxWidth: "100%", minWidth: "50%", borderRadius: 15}} width="90%" height="500" src="donoguide.html"></iframe>
+            <iframe src="https://scribehow.com/embed/SCD_Donation_Guide__FjgiwL1EQbiYCxuPbWXV_g" width="640" height="640" allowfullscreen frameborder="0"></iframe>
+
             {/* <PaypalEmbed /> */}
         </Box>
     )
