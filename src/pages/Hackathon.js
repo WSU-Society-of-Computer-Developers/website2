bfbc14f3669b385f4cad4a9ee1024a607faadf5f
ThatZiv <thatziv@gmail.com>
2024-08-29 19:37:24 -0400

    redid hackathon page

--- src/pages/Hackathon.js
+++ src/pages/Hackathon.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Box, /* Heading, FormField, Button, Paragraph, TextInput, TextArea, Form, Image */ } from "grommet";
+import {
+  Box /* Heading, FormField, Button, Paragraph, TextInput, TextArea, Form, Image */,
+} from "grommet";
 /* import { socials } from "../data";
 import Markdown from 'markdown-to-jsx/dist/index.js';
 import { Redirect } from "react-router-dom"; */
@@ -18,30 +20,75 @@ const futureEvents = `## We plan to compete in...
 `
  */
 
-// THIS SITE JUST REDIRECT TO WANYEHACKS.COM
 export default function Hackathon() {
-    return (
-        <Box flex overflow="auto" className="animate__animated animate__fadeInDown animate__fast" width="100%" align="center" gap="small" pad="small" style={{ color: "whitesmoke" }}>
-            <div className="card" style={{ background: "#02ff9c" }}>
-                {/* <Box
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <Box
+        flex
+        overflow="auto"
+        className="animate__animated animate__fadeInDown animate__fast md:w-full lg:w-1/2"
+        align="center"
+        gap="small"
+        pad="small"
+        style={{ color: "whitesmoke" }}
+      >
+        <div className="card bg-[#f0f4ec]">
+          {/* <Box
                     round="small" width="large"
                     height="medium" >
                     <Image style={{ borderRadius: 5 }} fit="cover" alt="wayne hacks banner" src="https://showcase.zavaar.net/pics/whacks.png" />
                 </Box> */}
-                <img src="https://showcase.zavaar.net/pics/whacks.png" className="img-fluid" alt="wayne hacks banner" />
-                <div className="container-fluid mt-2">
-                    <div className="btn-group w-100">
-                        <a target="_blank" rel="noopener noreferrer" href="https://waynehacks.com" className="btn btn-outline-success">More info</a>
-                        <a target="_blank" rel="noopener noreferrer" href="https://waynehacks.devpost.com" className="btn btn-outline-success">Devpost</a>
-                    </div>
-                </div>
-                <div className="card-body">
-                    WayneHacks is a 48-hour virtual hackathon hosted by
-                    Wayne State University. Hackers have 48 hours to complete a project related to the theme given at the opening ceremony.
-                    WayneHacks also hosts workshops and two gaming tournaments during the event! $5000 in prizes are given out at the end to the best hacks.
-                    <strong> For our Winter 2023 event, we had over 100 registrants, 50 active participants, and 16 project submissions.</strong>
-                </div>
+          <img
+            src="whacks3_banner.png"
+            className="img-fluid"
+            alt="wayne hacks banner"
+          />
+          <div className="container-fluid mt-2">
+            <p className="text-center animate-pulse">January 2025</p>
+            <a
+              href="https://waynehacks.com/login?signup=true"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className="btn font-bold btn-success w-full hover:py-4 transition-all">
+                Sign Up
+              </button>
+            </a>
+          </div>
+          <div className="card-body">
+            <a
+              href="https://waynehacks.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-500 hover:underline"
+            >
+              WayneHacks
+            </a>{" "}
+            is a 24-hour in-person Hackathon at Wayne State University. All
+            majors and skill levels are welcome with teams up to four people.
+            Prizes are awarded to the best projects.{" "}
+            <strong>Visit previous WayneHacks submissions:</strong>
+            <div className="btn-group w-100">
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://waynehacks.devpost.com"
+                className="btn btn-outline-success"
+              >
+                WayneHacks
+              </a>
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://waynehacks-2.devpost.com"
+                className="btn btn-outline-success"
+              >
+                WayneHacks 2
+              </a>
             </div>
-        </Box>
-    );
+          </div>
+        </div>
+      </Box>
+    </div>
+  );
 }
