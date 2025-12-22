be47cd1fffe82f87925d7bb44e56b086f617d5ad
Zavaar Shah <thatziv@gmail.com>
2025-12-22 13:38:16 -0500

    dead img

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,44 @@
-import React from "react"
-import { Anchor, Footer as Foot } from "grommet"
+import React from "react";
+import { Anchor, Footer as Foot } from "grommet";
 
 const stickyFooter = {
-    position: "fixed",
-    left: 0,
-    bottom: 0,
-    width: "100%",
-    backgroundColor: "red",
-    color: "white",
-    textAlign: "center"
-}
+  position: "fixed",
+  left: 0,
+  bottom: 0,
+  width: "100%",
+  backgroundColor: "red",
+  color: "white",
+  textAlign: "center",
+};
 
 export const Footer = () => {
-    return (<div style={stickyFooter}>
-        <Foot background='black' pad="small" align="center" as="footer" justify="between">
-            <a target="_blank" href="https://engineering.wayne.edu/" rel="noopener noreferrer">
-                <img alt="WSU Banner" className="Media" width="150" height="40vw" src="https://mac.wayne.edu/images/engineering_horz_color.png" />
-            </a>
-            <Anchor size="xsmall" label={`Copyright © ${new Date().getFullYear()} SCD`} />
-        </Foot>
-    </div>)
-}
+  return (
+    <div style={stickyFooter}>
+      <Foot
+        background="black"
+        pad="small"
+        align="center"
+        as="footer"
+        justify="between"
+      >
+        <a
+          target="_blank"
+          href="https://engineering.wayne.edu/"
+          rel="noopener noreferrer"
+        >
+          <img
+            alt="WSU Banner"
+            className="Media"
+            width="150"
+            height="40vw"
+            src="https://services.wayne.edu/TDPortal/BE/Logo/78/0"
+          />
+        </a>
+        <Anchor
+          size="xsmall"
+          label={`Copyright © ${new Date().getFullYear()} SCD`}
+        />
+      </Foot>
+    </div>
+  );
+};
